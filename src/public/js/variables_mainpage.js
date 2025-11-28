c95f85aa7a59bef2122de59dffa507be5ad123d0
Mohamed Mahmoud <myworkmoud@gmail.com>
2025-11-29 01:58:36 +0200

    add variables for mainpage file

---
+++ src/public/js/variables_mainpage.js
@@ -0,0 +1,24 @@
+// DOM Elements - Main Container
+const container = document.querySelector('#container');
+
+// Header Elements
+const header = document.querySelector('#header');
+const health = document.querySelector('#health');
+const stats = document.querySelector('#stats');
+const points = document.querySelector('#points');
+const kills = document.querySelector('#kills');
+const pauseMenu = document.querySelector('#pause-menu');
+
+// Maze Container Elements
+const mazeContainer = document.querySelector('#maze-container');
+const mazeContent = document.querySelector('#maze-content');
+const maze = document.querySelector('#maze');
+
+const leaderboardPanel = document.querySelector('#leaderboard-panel');
+
+// Footer Elements
+const footer = document.querySelector('#footer');
+const reload = document.querySelector('#reload');
+const howToPlay = document.querySelector('#how-to-play');
+const reloadCost = document.querySelector('#reload-cost');
+
