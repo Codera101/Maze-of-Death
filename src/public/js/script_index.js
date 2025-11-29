95cbeccb154aba1d4a2a42fef14989fbe532e5f8
Yousef-Gaber11 <138793695+Yousef-Gaber11@users.noreply.github.com>
2025-11-29 16:59:51 +0200

    Add script structure

---
+++ src/public/js/script_index.js
@@ -0,0 +1,14 @@
+const username = document.getElementById("username");
+const counter = document.getElementById("counter");
+const play_btn = document.getElementById("play_btn");
+const play_btn_container = document.getElementById("play_btn_container");
+
+//*********COUNTER****************/
+
+// ********PLAY_BUTTON**********/
+
+//*********POPUP_CONTROLLER*************/
+
+//***********TEST**************/
+
+
