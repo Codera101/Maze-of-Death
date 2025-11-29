0faef94647154c70faf4ce60e3fb168fdfd17104
Mohamed Mahmoud <myworkmoud@gmail.com>
2025-11-29 19:56:50 +0200

    draw player

--- src/public/js/maze.js
+++ src/public/js/maze.js
@@ -3,15 +3,16 @@ const app = new PIXI.Application();
 
 // Must await init()
 await app.init({
-    width: cols * cellSize,
-    height: rows * cellSize,
-    background: '#1e1e1e'
+    width: cols * cellSize + (cols - 1) * strokeWidth * 2,
+    height: rows * cellSize + (rows - 1) * strokeWidth * 2,
+    background: '#0D0D16',
+
 });
 
 /* ***************************** ****************************** *********************** */
 
 // function to create a rounded rectangle with fill and stroke
-function createRoundedRect({
+function drawRoundedRect({
     x = 0,
     y = 0,
     width = 100,
@@ -51,51 +52,113 @@ function createRoundedRect({
     return g;
 }
 
+function drawCircle({
+    x = 0,
+    y = 0,
+    radius = 50,
+    fillColor = 0xffffff,
+    strokeColor = 0x000000,
+    strokeWidth = 0
+}) {
+    const g = new PIXI.Graphics();
+
+    // BORDER INSIDE: reduce radius by half of strokeWidth
+    const r = radius - strokeWidth / 2;
+
+    // Fill
+    g.beginFill(fillColor);
+    g.drawCircle(x, y, r);
+    g.endFill();
+
+    // Border
+    if (strokeWidth > 0) {
+        g.lineStyle(strokeWidth, strokeColor, 1, 0, false, false);
+        g.drawCircle(x, y, r);
+    }
+
+    app.stage.addChild(g);
+    return g;
+}
+
 
 function drawMaze() {
-    for (let row = 0; row < rows * cellSize; row += cellSize) {
-        for (let col = 0; col < cols * cellSize; col += cellSize) {
-            createRoundedRect({
-                x: col,
-                y: row,
-                width: cellSize,
-                height: cellSize,
-                radius: 5,
-                fillColor: 0x808080,
-                strokeColor: 0x000000,
-                strokeWidth: 2
-            });
+    for (let row = 0; row < rows * cellSize + (rows - 1) * strokeWidth; row += cellSize + strokeWidth * 2) {
+        for (let col = 0; col < cols * cellSize + (cols - 1) * strokeWidth; col += cellSize + strokeWidth * 2) {
+
+            if (mazeLayout[row / (cellSize + strokeWidth * 2)][col / (cellSize + strokeWidth * 2)] === 0) {
+                drawRoundedRect({
+                    x: col,
+                    y: row,
+                    width: cellSize,
+                    height: cellSize,
+                    radius: 1,
+                    fillColor: "#1a1a28",
+                    strokeColor: "#FF2C47",
+                    strokeWidth: strokeWidth  
+                });
+            } else {
+                drawRoundedRect({
+                    x: col,
+                    y: row,
+                    width: cellSize,
+                    height: cellSize,
+                    radius: 1,
+                    fillColor: "#0D0D16",
+                    strokeColor: "#10212A",
+                    strokeWidth: strokeWidth
+                })
+            }
         }
     }
 } 
 
-function drawPlayer() {
+function drawPlayer({
+    x = 0,
+    y = 0,
+    width = cellSize,
+    height = cellSize,
+    radius = 7,
+    dir = "up",
+    fillColor,
+}) {
+    drawRoundedRect({x, y, width: width, height: height, radius: 15, fillColor: fillColor, strokeColor: "#FFFFFF", strokeWidth: 0});
+    let cx, cy;
     
+    if (dir === 'up' || dir === 'down') {
+        cx = x + width / 2;
+        cy = dir === 'up' ? y + 15 : y + height - 15;
+    } else if (dir === 'left' || dir === 'right') {
+        cx = dir === 'left' ? x + 15 : x + width - 15;
+        cy = y + height / 2;
+    }
+    drawCircle({x: cx, y: cy, radius: radius, fillColor: "#fff", strokeColor: "#FFFFFF", strokeWidth: 2});
+}
+
+function drawPlayers() {
+    if (visiblePlayers.length === 0) return;
+    visiblePlayers.forEach(player => {
+        drawPlayer({
+            x: player.x,
+            y: player.y,
+            dir: player.dir,
+            fillColor: player.fillColor
+        });
+    });
 }
 
 
 drawMaze();
 
+drawPlayer({
+    x: 0 + (cellSize + strokeWidth * 2) * 3,
+    y: 0 + (cellSize + strokeWidth * 2) * 5,
+    dir: 'right',
+    fillColor: "#4CAF50"
+});
 
 
 
 
-
-
-
-
-
-// app.stage.addChild();
-// createRoundedRect({
-//     x: 50,
-//     y: 50,
-//     width: 100,
-//     height: 100,
-//     radius: 20,
-//     fillColor: 0x00ff00,
-//     strokeColor: 0x0000ff,  
-//     strokeWidth: 5
-// })
 
 
 mazeContent.appendChild(app.canvas);
