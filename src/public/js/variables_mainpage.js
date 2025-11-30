85b9575457dc7ba46bc06109d2c716bcbce24a77
Ahmed M. Wahba <ahmedz.mohameda69@gmail.com>
2025-11-30 19:51:10 +0200

    bugfix/switching scene

--- src/public/js/variables_mainpage.js
+++ src/public/js/variables_mainpage.js
@@ -30,7 +30,7 @@ const reloadCost = document.querySelector("#reload-cost");
 const killMessage = document.querySelector("#kill-message");
 
 // Socket Connection
-// const socket = io("http://localhost:3000");
+const socket = io("http://localhost:3000");
 
 // Maze Variables
 let rows = 16;
