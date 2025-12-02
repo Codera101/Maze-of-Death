8fc9da3dab6c5b339bc877c6cfeda63c6ddfad12
Mazen-Ghanaym <mznghanayem@gmail.com>
2025-12-02 17:55:01 +0200

    test commit for deploy

--- src/public/js/variables_mainpage.js
+++ src/public/js/variables_mainpage.js
@@ -32,7 +32,7 @@ const killMessage = document.querySelector("#kill-message");
 // Socket Connection
 // const socket = io("http://localhost:3000");
 // const socket = io("http://localhost:8080");
-const socket = io("https://maze-of-death-production.up.railway.app/");
+const socket = io("https://maze-of-death-production.up.railway.app");
 
 // Maze Variables
 let rows = 16;
