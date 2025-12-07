ef43a635edc3489882eda0e71e49735a95111ed9
Ahmed M. Wahba <ahmedz.mohameda69@gmail.com>
2025-12-07 11:01:17 +0200

    applied antialiasing for round corners

--- src/public/js/maze.js
+++ src/public/js/maze.js
@@ -6,5 +6,8 @@ await app.init({
     width: cols * cellSize + (cols - 1) * strokeWidth * 2,
     height: rows * cellSize + (rows - 1) * strokeWidth * 2,
     background: '#0D0D16',
+    antialias: true,
+    autoDensity:true,
+    resolution: window.devicePixelRatio || 1,
 });
 mazeContent.appendChild(app.canvas);
