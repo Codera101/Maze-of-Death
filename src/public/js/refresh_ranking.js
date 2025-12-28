123937dc4ebba78db1494ced90b07f42cabd17cc
Ahmed M. Wahba <ahmedz.mohameda69@gmail.com>
2025-12-28 04:39:59 +0200

    early break in ranking self

--- src/public/js/refresh_ranking.js
+++ src/public/js/refresh_ranking.js
@@ -36,13 +36,18 @@ socket.on("refresh_ranking", ({ all_players }) => {
     }
     fragment.appendChild(newPlayer);
   };
+  let selfAdded = false;
   for (let i = 0; i < limit; i++) {
     addPlayer(all_players[i], i + 1);
+    if (all_players[i].username == myPlayer.username) {
+      selfAdded = true;
+    }
   }
-  for (let i = 0; i < all_players.length; i++) {
+  for (let i = 0; i < all_players.length && !selfAdded; i++) {
     if (all_players[i].username == myPlayer.username) {
       if (i >= limit) {
         addPlayer(all_players[i], i + 1);
+        selfAdded = true;
       }
     }
   }
